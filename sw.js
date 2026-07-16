36e5e6bfebd8c465704ce74051a23bed6fd91d88
gatamylat <putyn.k.a@gmail.com>
2026-07-16 19:54:59 +0300

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Латимер 2 · service worker · network-first, офлайн из кэша */
-const CACHE='latimer2-v1';
+const CACHE='latimer2-v2';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).catch(()=>{}));
   self.skipWaiting();
